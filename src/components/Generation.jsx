c80500be6422089c4a7f46065bc2a143a4f9389a
Naveed-islam-007 <islamnaveed631@gmail.com>
2026-05-02 07:50:51 +0600

    fix

--- src/components/Generation.jsx
+++ src/components/Generation.jsx
@@ -7,7 +7,7 @@ const Generation = async () => {
    const hotest= await getHotest()
   return (
     <div className="container mx-auto py-10 px-4">
-      <h2 className="text-3xl font-bold mb-6 ">Courses</h2>
+      <h2 className="text-3xl font-bold mb-6 ">Trending Courses</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {hotest.map((course) => (
          <CourseCard key={course.id} course={course}></CourseCard>
